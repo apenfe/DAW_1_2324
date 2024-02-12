24fb501f797b9fc75274fe481bb0ab78ac75f162
apenfe <adrian1414@hotmail.es>
2024-02-12 12:26:37 +0100

    12-02-2024 Avance de juego 2048 -sin terminar-

--- LOGICA/src/juego2048Poo06/Main.java
+++ LOGICA/src/juego2048Poo06/Main.java
@@ -1,9 +1,116 @@
 package juego2048Poo06;
 
+import java.util.Scanner;
+
 public class Main{
+	
+	public static Scanner teclado = new Scanner(System.in);
 	
 	public static void main(String[] args){
 		
+		Tablero actual = new Tablero();
+		
+		do {
+			
+			System.out.println("--- MENU ---");
+			System.out.println("1 - INICIAR PARTIDA");
+			System.out.println("2 - CONTINUAR PARTIDA");
+			System.out.println("3 - SALIR");
+			int opcion = pedirInt("Seleccione una opcion: ");
+			
+			if(opcion==3) {
+				
+				System.out.println("Fin del programa.");
+				break;
+				
+			}else if(opcion==2) {
+				
+				if(!actual.isInit()) {
+					
+					System.out.println("Primero debe de iniciar una partida.");
+					
+				}else {
+					
+					do {
+						
+						actual.mostrar();
+						
+						System.out.print("Intriduce un movimiento (a,d,w,s) (m - menu): ");
+						String movimiento = teclado.nextLine().trim().toLowerCase();
+						
+						if(movimiento.equals("m")) {
+							
+							System.out.println("Volviendo al menu principal.");
+							break;
+							
+						}else if(movimiento.equals("a")||movimiento.equals("d")||movimiento.equals("w")||movimiento.equals("s")) {
+							
+						}else {
+							
+							System.out.println("Inserte un movimeinto valido.");
+							
+						}
+						
+					}while(true);
+					
+				}
+				
+			}else if(opcion==1) {
+				
+				actual = new Tablero(true);
+				
+				do {
+					
+					actual.mostrar();
+					
+					System.out.print("Intriduce un movimiento (a,d,w,s) (m - menu): ");
+					String movimiento = teclado.nextLine().trim().toLowerCase();
+					
+					if(movimiento.equals("m")) {
+						
+						System.out.println("Volviendo al menu principal.");
+						break;
+						
+					}else if(movimiento.equals("a")||movimiento.equals("d")||movimiento.equals("w")||movimiento.equals("s")) {
+						
+					}else {
+						
+						System.out.println("Inserte un movimeinto valido.");
+						
+					}
+					
+				}while(true);
+				
+			}else {
+				
+				System.out.println("Seleccione una opcion valida entre 1 y 3.");
+				
+			}
+			
+		}while(true);
+		
+	}
+	
+	public static int pedirInt(String enunciado) {
+		
+		System.out.print(enunciado);
+		
+		int salida = 0;
+		
+		do {
+			
+			try {
+				
+				salida = Integer.parseInt(teclado.nextLine().trim());
+				return salida;
+				
+			} catch (Exception e) {
+				
+				System.err.print("ERROR - Inserte un numero entero: ");
+				
+			}
+			
+		} while (true);
 		
 	}
 	
--- LOGICA/src/juego2048Poo06/Tablero.java
+++ LOGICA/src/juego2048Poo06/Tablero.java
@@ -2,6 +2,130 @@ package juego2048Poo06;
 
 public class Tablero{
 	
+	private final int FIL = 4;
+	private final int COL = 4;
+	private int[][] tablero = new int[FIL][COL];
+	private boolean init;
+	
+	public Tablero() {
+		
+	}
+	
+	public Tablero(boolean check) {
+		
+		for (int i = 0; i < FIL; i++) {
+			for (int j = 0; j < COL; j++) {
+				tablero[i][j]=0;
+			}
+		}
+		
+		int i1, j1, i2, j2;
+		
+		do {
+			
+			i1=(int)(Math.random()*FIL);
+			j1=(int)(Math.random()*COL);
+			i2=(int)(Math.random()*FIL);
+			j2=(int)(Math.random()*COL);	
+			
+		}while(i1==i2 && j1==j2);
+		
+		tablero[i1][j1]=2;
+		tablero[i2][j2]=2;
+		
+		this.init=true;
+		
+	}
+	
+	public void mostrar() {
+		
+		System.out.println();
+		
+		for (int i = 0; i < FIL; i++) {
+			
+			for (int j = 0; j < COL; j++) {
+				
+				System.out.print(tablero[i][j]+" ");
+				
+			}
+			
+			System.out.println();
+
+		}
+		
+		System.out.println();
+		
+	}
+	
+	public void mover(String direccion) {
+		
+		for (int i = 0; i < FIL; i++) {
+			
+			for (int j = 0; j < COL; j++) {
+				
+				System.out.print(tablero[i][j]+" ");
+				
+			}
+			
+			System.out.println();
+
+		}
+		
+	}
+	
+	private boolean ganador() {
+		
+		for (int i = 0; i < FIL; i++) {
+			
+			for (int j = 0; j < COL; j++) {
+				
+				if(tablero[i][j]==2048) {
+					return true;
+				}
+				
+			}
+			
+		}
+		
+		return false;
+		
+	}
+	
+	public boolean finPartida() {
+		
+		if(ganador()) {
+			
+			System.out.println("HAS GANADO.");
+			return true;
+			
+		}else {
+			
+			for (int i = 0; i < FIL; i++) {
+				
+				for (int j = 0; j < COL; j++) {
+					
+					if(tablero[i][j]==0) {
+						return false;
+					}
+					
+				}
+				
+			}
+			
+		}
+		
+		return false;
+		
+	}
+
+	public boolean isInit() {
+		return init;
+	}
+
+	public void setInit(boolean init) {
+		this.init = init;
+	}
+	
 	
 	
 }
