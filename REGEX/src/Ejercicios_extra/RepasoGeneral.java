9e3838ce3aa015bab083f520680770401327fcaa
apenfe <adrian1414@hotmail.es>
2024-02-19 10:08:29 +0100

    19-02-2024 (DAW) Termino Repaso general ejercicios internet REGEX

--- REGEX/src/Ejercicios_extra/RepasoGeneral.java
+++ REGEX/src/Ejercicios_extra/RepasoGeneral.java
@@ -8,28 +8,28 @@ public class RepasoGeneral {
 
 	public static void main(String[] args) {
 
-		//Binario();
-		//Dni();
-		//Email();
-		//EnteroNegativo();
-		//EnteroPositivo();
-		//Fecha();
-		//Hexadecimal();
-		//Instagram();
-		//IP();
-		//ISBN();
-		//Matricula();
-		//NombresCompuestos();
-		//NuemeroEntero();
-		//Octal();
-		//PasswordCompleja();
-		//PasswordSimple();
-		//Real();
-		//RealConXDecimales(4);
-		//RealNegativo();
-		//RealNegativoConXDecimales(2);
-		//RealPositivo();
-		//RealPositivoConXDecimales();
+		Binario();
+		Dni();
+		Email();
+		EnteroNegativo();
+		EnteroPositivo();
+		Fecha();
+		Hexadecimal();
+		Instagram();
+		IP();
+		ISBN();
+		Matricula();
+		NombresCompuestos();
+		NuemeroEntero();
+		Octal();
+		PasswordCompleja();
+		PasswordSimple();
+		Real();
+		RealConXDecimales(4);
+		RealNegativo();
+		RealNegativoConXDecimales(2);
+		RealPositivo();
+		RealPositivoConXDecimales(3);
 
 	}
 
@@ -68,7 +68,7 @@ public class RepasoGeneral {
 		System.out.print("Inserte un email: ");
 		String texto = teclado.nextLine().trim();
 		
-		String regex="";
+		String regex="^([0-9A-Za-z-_]+)@([0-9A-Za-z-_]+)\\.([a-z]{2,4})$";
 		
 		if(texto.matches(regex)) {
 			System.out.println("Coincide");
@@ -110,10 +110,10 @@ public class RepasoGeneral {
 
 	public static void Fecha() {
 		
-		System.out.print("Inserte una fecha: ");
+		System.out.print("Inserte una fecha (dd/mm/aaaa): ");
 		String texto = teclado.nextLine().trim();
 		
-		String regex="";
+		String regex="^(0?[1-9]{1}|[12]{1}[0-9]{1}|3[0-1])/(0?[1-9]{1}|1[0-2]{1})/([12]{1}[0-9]{3})$";
 		
 		if(texto.matches(regex)) {
 			System.out.println("Coincide");
@@ -155,10 +155,10 @@ public class RepasoGeneral {
 
 	public static void IP() {
 		
-		System.out.print("Inserte numero binario: ");
+		System.out.print("Inserte una IP: ");
 		String texto = teclado.nextLine().trim();
 		
-		String regex="";
+		String regex="^(([\\d]{1}|[1-9]{1}[0-9]{1}|1[0-9]{2}|2[0-4]{1}[0-9]{1}|2[0-5]{2})(\\.)){3}([\\d]{1}|[1-9]{1}[0-9]{1}|1[0-9]{2}|2[0-4]{1}[0-9]{1}|2[0-5]{2}){1}$";
 		
 		if(texto.matches(regex)) {
 			System.out.println("Coincide");
@@ -215,10 +215,10 @@ public class RepasoGeneral {
 
 	public static void NombresCompuestos() {
 		
-		System.out.print("Inserte numero binario: ");
+		System.out.print("Inserte un nombre compuesto: ");
 		String texto = teclado.nextLine().trim();
 		
-		String regex="";
+		String regex="^([A-ZÁÉÍÓÚÑ]{1}[a-záéíóúñ]+[ ]?){2,3}$";
 		
 		if(texto.matches(regex)) {
 			System.out.println("Coincide");
@@ -230,10 +230,10 @@ public class RepasoGeneral {
 
 	public static void NuemeroEntero() {
 		
-		System.out.print("Inserte numero binario: ");
+		System.out.print("Inserte numero entero: ");
 		String texto = teclado.nextLine().trim();
 		
-		String regex="";
+		String regex="^-?[\\d]+$";
 		
 		if(texto.matches(regex)) {
 			System.out.println("Coincide");
@@ -260,10 +260,19 @@ public class RepasoGeneral {
 
 	public static void PasswordCompleja() {
 		
-		System.out.print("Inserte numero binario: ");
+		/*
+		 * Al menos 10 caracteres de longitud.
+		 * Al menos una letra mayúscula.
+		 * Al menos una letra minúscula.
+		 * Al menos un dígito.
+		 * Al menos un carácter especial (como @, #, $, etc.).
+		 * 
+		 */
+		
+		System.out.print("Inserte una contraseña compleja, digito, mayus, minus y simbolo especial, minimo 10: ");
 		String texto = teclado.nextLine().trim();
 		
-		String regex="";
+		String regex="^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@#\\$]).{10,}$";
 		
 		if(texto.matches(regex)) {
 			System.out.println("Coincide");
@@ -275,10 +284,20 @@ public class RepasoGeneral {
 
 	public static void PasswordSimple() {
 		
-		System.out.print("Inserte numero binario: ");
+		/*
+		 * Debe tener al menos 8 caracteres.
+		 * Debe contener al menos una letra mayúscula.
+		 * Debe contener al menos una letra minúscula.
+		 * Debe contener al menos un dígito.
+		 * 
+		 */
+		
+		System.out.print("Inserte una contraseña, minimo 1 mayuscula, 1 minuscula, 1 digito, extension minima 8: ");
 		String texto = teclado.nextLine().trim();
 		
-		String regex="";
+		/* (?=.*[...]) ---> LOOKHEAD BUSCA AL MENOS UNA COINCIDENCIA*/ 
+		
+		String regex="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$";
 		
 		if(texto.matches(regex)) {
 			System.out.println("Coincide");
