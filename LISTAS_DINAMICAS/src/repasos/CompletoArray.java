33fbcd9dde4835f1a5a3679d66261d434255e463
apenfe <adrian1414@hotmail.es>
2024-03-08 13:42:16 +0100

    08-03-2024 (DAW) Repaso completo de listas

--- LISTAS_DINAMICAS/src/repasos/CompletoArray.java
+++ LISTAS_DINAMICAS/src/repasos/CompletoArray.java
@@ -77,7 +77,7 @@ public class CompletoArray{
 		
 		comprobarLlenado();
 		
-		if(indice > 0) { // o indice < numElementos
+		if(indice < numElementos) { 
 			
 			System.arraycopy(arrayElementos, indice, arrayElementos, indice+1, numElementos-indice);
 			
@@ -276,6 +276,295 @@ public class CompletoArray{
 	}
 	
 	public Object peekCola() {
+		
+		if(numElementos>0) {
+			return arrayElementos[0];
+		}
+		
+		return null;
+		
+	}
+	
+	// ------------------- REPASO 2 ----------------------------------------------------
+	
+	/*
+	 * ** void add(Object) 		- Añade un elemento al final de la lista.
+	 * ** void add(int, Object) 		- Inserta un elemento en la posición indicada por un índice.
+	 * ** void comprobarLlenado()
+	 * ** void clear() 			- Elimina todos los elementos de la lista.
+	 * ** boolean contains(Object) 	- Comprueba si el elemento está incluido en la lista.
+	 * ** Object get(int) 			- Obtiene el elemento de la posición indicada por índice.
+	 * ** int indexOf(Object) 		- Devuelve la posición del elemento.
+	 * ** boolean isEmpty() 		- Comprueba si la lista está vacía.
+	 * ** boolean remove(Object) 	- Elimina el elemento correspondiente.
+	 * ** Object remove(int) 		- Elimina el elemento de la posición indicada por índice.
+	 * ** int size() 			- Número de elementos de la estructura.
+	 * void removeAll(Object[])
+	 * 
+	 */
+	
+	public void removeAllV2(CompletoArray lista) {
+		
+		for (int i = 0; i < lista.size(); i++) {
+			remove(lista.get(i));
+		}
+		
+	}
+	
+	public Object removeV2(int indice) { // repasar he fallado
+		
+		if(indice<0||indice>=numElementos) {
+			throw new IndexOutOfBoundsException("ERROR");
+		}
+		
+		Object salida = arrayElementos[indice];
+		
+		System.arraycopy(arrayElementos, indice+1, arrayElementos, indice, numElementos - (indice+1));
+		
+		arrayElementos[numElementos-1]=null;
+		numElementos--;
+		
+		return salida;
+		
+	}
+	
+	public boolean removeV2(Object dato) {
+		
+		int indice = indexOfV2(dato);
+		
+		if(indice!=-1) {
+			remove(indice);
+			return true;
+		}
+		
+		return false;
+		
+	}
+	
+	public void addV2(int indice, Object dato) { // repasar he fallado
+		
+		if(indice<0||indice>=numElementos) {
+			throw new IndexOutOfBoundsException("ERROR");
+		}
+		
+		comprobarLlenadoV2();
+		
+		if(numElementos>indice) {
+			
+			System.arraycopy(arrayElementos, indice, arrayElementos, indice+1, numElementos-indice);
+			
+		}
+		
+		arrayElementos[indice]=dato;
+		numElementos++;
+		
+	}
+	
+	public void addV2(Object dato) {
+		
+		if(numElementos==0) {
+			
+			arrayElementos[0]=dato;
+			
+		}else {
+			
+			comprobarLlenado();
+			arrayElementos[numElementos]=dato;	
+			
+		}
+		
+		numElementos++;
+		
+	}
+	
+	private void comprobarLlenadoV2() {
+		
+		if(numElementos+1==arrayElementos.length) {
+			
+			Object[] ampliado = new Object[arrayElementos.length*2];
+			System.arraycopy(arrayElementos, 0, ampliado, 0, numElementos);
+			arrayElementos=ampliado;
+			
+		}
+		
+	}
+	
+	public int sizeV2() {
+		
+		return numElementos;
+		
+	}
+	
+	public void clearV2() {
+		
+		this.arrayElementos=new Object[NUM_INICIAL];
+		this.numElementos=0;
+		
+	}
+	
+	public boolean isEmptyV2() {
+		
+		return numElementos==0;
+		
+	}
+	
+	public int indexOfV2(Object dato) {
+		
+		if(dato==null) {
+			
+			for (int i = 0; i < numElementos; i++) {
+				
+				if(dato==arrayElementos[i]) {
+					return i;
+				}
+				
+			}
+			
+		}else {
+			
+			for (int i = 0; i < numElementos; i++) {
+				
+				if(dato.equals(arrayElementos[i])) {
+					return i;
+				}
+				
+			}
+			
+		}
+		
+		return -1;
+		
+	}
+	
+	public boolean containsV2(Object dato) {
+		
+		int indice = indexOfV2(dato);
+		
+		if(indice!=-1) {
+			return true;
+		}
+		
+		return false;
+		
+	}
+	
+	public Object getV2(int indice) {
+		
+		if(indice<0||indice>=numElementos) {
+			throw new IndexOutOfBoundsException("ERROR");
+		}
+		
+		return arrayElementos[indice];	
+		
+	}
+	
+	/*
+	 * METODOS DE PILA / STACK
+	 * 
+	 * ** Object[] toArray()- Devuelve un array con todos los elementos de la pila.
+	 * ** Object peek() 			- Obtiene el elemento de la cima de la pila sin quitarlo.
+	 * ** Object pop() 			- Extrae el elemento situado en la cima de la pila.
+	 * ** void push(Object) 		- Añade un elemento en la cima de la pila.
+	 * 
+	 */
+	
+	public Object peekV2() {
+		
+		if(numElementos>0) {
+			return arrayElementos[numElementos-1];
+		}
+		
+		return null;
+		
+	}
+	
+	public Object popV2() {
+		
+		if(numElementos>0) {
+			
+			Object salida = arrayElementos[numElementos-1];
+			arrayElementos[numElementos-1]=null;
+			numElementos--;
+			return salida;
+			
+		}
+		
+		return null;
+		
+	}
+	
+	public void pushV2(Object dato) {
+		
+		if(numElementos==0) {
+			
+			arrayElementos[0]=dato;
+			
+		}else {
+			
+			comprobarLlenado();
+			arrayElementos[numElementos]=dato;	
+			
+		}
+		
+		numElementos++;
+		
+	}
+	
+	public Object[] toArrayV2() {
+		
+		Object[] salida = new Object[numElementos];
+		
+		for (int i = 0; i < salida.length; i++) {
+			salida[i]=arrayElementos[i];
+		}
+		
+		return salida;
+		
+	}
+	
+	/*
+	 * METODOS DE COLA / QUEUE
+	 * 
+	 * ** Object peekCola() 			- Obtiene el elemento de la cabeza de la cola sin quitarlo.
+	 * ** Object poll() 			- Extrae el elemento situado en la cabeza de la cola.
+	 * ** void offer(Object) 		- Añade un elemento al final de la cola.
+	 * 
+	 */
+	
+	public Object pollV2() { // REPASAR
+		
+		if(numElementos>0) {
+			
+			Object salida = arrayElementos[0];
+			System.arraycopy(arrayElementos, 1, arrayElementos, 0, numElementos-1);
+			arrayElementos[numElementos-1]=null;
+			numElementos--;
+			return salida;
+			
+		}
+		
+		return null;
+		
+	}
+	
+	public void offerV2(Object dato) {
+		
+		if(numElementos==0) {
+			
+			arrayElementos[0]=dato;
+			
+		}else {
+			
+			comprobarLlenado();
+			arrayElementos[numElementos]=dato;	
+			
+		}
+		
+		numElementos++;
+		
+	}
+	
+	public Object peekColaV2() {
 		
 		if(numElementos>0) {
 			return arrayElementos[0];
--- LISTAS_DINAMICAS/src/repasos/CompletoNodo.java
+++ LISTAS_DINAMICAS/src/repasos/CompletoNodo.java
@@ -1,5 +1,7 @@
 package repasos;
 
+import listasEnlazadas.Nodo;
+
 public class CompletoNodo{
 	
 	private int numElementos;
@@ -283,6 +285,376 @@ public class CompletoNodo{
 		Node ultimo = obtenerNodo(numElementos-1);
 		
 		ultimo.siguiente=nuevo;
+		numElementos++;
+		
+	}
+	
+	// --------------------------- Repaso 2 -------------------------------
+	
+	/*
+	 * ** void add(Object) 		- Añade un elemento al final de la lista.
+	 * ** void add(int, Object) 		- Inserta un elemento en la posición indicada por un índice.
+	 * ** Nodo obtenerNodo(int indice)
+	 * ** void clear() 			- Elimina todos los elementos de la lista.
+	 * ** boolean contains(Object) 	- Comprueba si el elemento está incluido en la lista.
+	 * ** Object get(int) 			- Obtiene el elemento de la posición indicada por índice.
+	 * ** int indexOf(Object) 		- Devuelve la posición del elemento.
+	 * ** boolean isEmpty() 		- Comprueba si la lista está vacía.
+	 * ** boolean remove(Object) 	- Elimina el elemento correspondiente.
+	 * ** Object remove(int) 		- Elimina el elemento de la posición indicada por índice.
+	 * ** Object removePrimero()
+	 * ** Object removeIntermedio(int)
+	 * ** int size() 			- Número de elementos de la estructura.
+	 * ** void removeAll(Object[])
+	 * 
+	 */
+	
+	public void removeAllV2(CompletoNodo lista) {
+		
+		for (int i = 0; i < lista.size(); i++) {
+			remove(lista.get(i));
+		}
+		
+	}
+	
+	public Object removeV2(int indice) { 
+		
+		if(indice<0||indice>=numElementos) {
+			throw new IndexOutOfBoundsException("ERROR");
+		}
+		
+		if(indice==0) {
+			return removePrimeroV2();
+		}
+		
+		if(indice>0) {
+			return removeIntermedioV2(indice);
+		}
+		
+		return null;
+		
+	}
+	
+	public Object removePrimeroV2() { 
+		
+		if(numElementos>0) {
+			
+			Node actual = primero;
+			Node sig = actual.siguiente;
+			primero=sig;
+			
+			numElementos--;
+			return actual.dato;
+			
+		}
+		
+		return null;	
+		
+	}
+	
+	public Object removeIntermedioV2(int indice) { 
+		
+		if(indice<0||indice>=numElementos) {
+			throw new IndexOutOfBoundsException("ERROR");
+		}
+		
+		Node ant = obtenerNodo(indice-1);
+		Node actual = ant.siguiente;
+		
+		ant.siguiente=actual.siguiente;
+		numElementos--;
+		
+		return actual.dato;
+		
+	}
+	
+	public boolean removeV2(Object dato) {
+		
+		int indice = indexOfV2(dato);
+		
+		if(indice!=-1) {
+			remove(indice);
+			return true;
+		}
+		
+		return false;
+	
+	}
+	
+	public void addV2(int indice, Object dato) { 
+		
+		if(indice<0||indice>=numElementos) {
+			throw new IndexOutOfBoundsException("ERROR");
+		}
+		
+		Node nuevo = new Node(dato);
+		
+		if(indice==0) {
+			
+			nuevo.siguiente=primero;
+			primero=nuevo;
+			numElementos++;
+			
+		}else if(indice==numElementos-1) {
+			
+			addV2(dato);
+			
+		}else {
+			
+			Node ant = obtenerNodo(indice-1);
+			Node actual = ant.siguiente;
+			
+			ant.siguiente=nuevo;
+			nuevo.siguiente=actual;
+			numElementos++;
+		}
+		
+	}
+	
+	public void addV2(Object dato) {
+		
+		Node nuevo = new Node(dato);
+		
+		if(numElementos==0) {
+			
+			primero=nuevo;
+			
+		}else {
+			
+			Node actual = obtenerNodo(numElementos-1);
+			actual.siguiente=nuevo;
+			
+		}
+		
+		numElementos++;
+		
+	}
+	
+	public Node obtenerNodoV2(int indice) {
+		
+		if(indice<0||indice>=numElementos) {
+			throw new IndexOutOfBoundsException("ERROR");
+		}
+		
+		Node actual = primero;
+		
+		for (int i = 0; i < indice; i++) {
+			
+			actual=actual.siguiente;
+			
+		}
+		
+		return actual;
+		
+	}
+	
+	public int sizeV2() {
+		
+		return numElementos;
+		
+	}
+	
+	public void clearV2() {
+		
+		this.primero=null;
+		this.numElementos=0;
+		
+	}
+	
+	public boolean isEmptyV2() {
+		
+		return numElementos==0;
+		
+	}
+	
+	public int indexOfV2(Object dato) { // repasar esta forma
+		
+		Node actual = primero;
+		
+		for (int i = 0; actual!=null; i++) {
+			
+			if((actual.dato!=null && actual.dato.equals(dato))||(dato==actual.dato)) {
+				return i;
+			}
+			
+			actual=actual.siguiente;
+			
+		}
+		
+		return -1;
+		
+	}
+	
+	public boolean containsV2(Object dato) {
+		
+		int indice = indexOfV2(dato);
+		
+		if(indice!=-1) {
+			return true;
+		}
+		
+		return false;
+		
+	}
+	
+	public Object getV2(int indice) {
+		
+		if(indice<0||indice>=numElementos) {
+			throw new IndexOutOfBoundsException("ERROR");
+		}
+		
+		if(numElementos>0) {
+			
+			Node salida = obtenerNodoV2(indice);
+			
+			return salida.dato;
+			
+		}
+		
+		return null;
+		
+	}
+	
+	/*
+	 * METODOS DE PILA / STACK
+	 * 
+	 * ** Object[] toArray()- Devuelve un array con todos los elementos de la pila.
+	 * ** Object peek() 			- Obtiene el elemento de la cima de la pila sin quitarlo.
+	 * ** Object pop() 			- Extrae el elemento situado en la cima de la pila.
+	 * ** void push(Object) 		- Añade un elemento en la cima de la pila.
+	 * 
+	 */
+	
+	public Object[] toArrayV2() {
+		
+		Object[] salida = new Object[numElementos];
+		
+		Node actual = primero;
+		
+		for (int i = 0; actual!=null; i++) {
+			
+			salida[i]=actual.dato;
+			actual=actual.siguiente;
+			
+		}
+		
+		return salida;
+		
+	}
+	
+	public Object peekV2() {
+		
+		if(numElementos>0) {
+			
+			Node ultimo = obtenerNodo(numElementos-1);
+			return ultimo.dato;
+			
+		}
+		
+		return null;
+		
+	}
+	
+	public Object popV2() {
+		
+		if(numElementos==0) {
+			
+			return null;
+			
+		}else if(numElementos==1) {
+			
+			Node salida = primero;
+			
+			primero=null;
+			numElementos=0;
+			
+			return salida.dato;
+			
+		}else {
+			
+			Node penultimo = obtenerNodo(numElementos-2);
+			Node ultimo = penultimo.siguiente;
+			
+			penultimo.siguiente=null;
+			
+			numElementos--;
+			return ultimo.dato;
+			
+		}
+		
+	}
+	
+	public void pushV2(Object objeto) {
+		
+		Node nuevo = new Node(objeto);
+		
+		if(numElementos==0) {
+			
+			primero=nuevo;
+			
+		}else {
+			
+			Node ultimo = obtenerNodo(numElementos-1);
+			ultimo.siguiente=nuevo;
+			
+		}
+		
+		numElementos++;
+	
+	}
+	
+	/*
+	 * METODOS DE COLA / QUEUE
+	 * 
+	 * ** Object peek() 			- Obtiene el elemento de la cabeza de la cola sin quitarlo.
+	 * ** Object poll() 			- Extrae el elemento situado en la cabeza de la cola.
+	 * ** void offer(Object) 		- Añade un elemento al final de la cola.
+	 * 
+	 */
+	
+	public Object peekCola() {
+		
+		if(numElementos>0) {
+			
+			return primero.dato;
+			
+		}
+		
+		return null;
+		
+	}
+	
+	public Object pollV2() {
+		
+		if(numElementos>0) {
+			
+			Node actual = primero;
+			primero = actual.siguiente;
+			numElementos--;
+			
+			return actual.dato;
+			
+		}
+		
+		return null;
+		
+	}
+	
+	public void offerV2(Object dato) {
+		
+		Node nuevo = new Node(dato);
+		
+		if(numElementos==0) {
+			
+			primero=nuevo;
+			
+		}else {
+			
+			Node actual = obtenerNodo(numElementos-1);
+			actual.siguiente=nuevo;
+			
+		}
+		
 		numElementos++;
 		
 	}
