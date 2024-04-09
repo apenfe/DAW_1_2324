b2222060dd3084b17d4bf4005d6c043cb82799f7
apenfe <adrian1414@hotmail.es>
2024-04-09 10:53:59 +0200

    09-04-2024 (DAW) BD_BLOQUE_1

--- PRACTICA_7.1/src/ejercicio_5y6/CdAudio.java
+++ PRACTICA_7.1/src/ejercicio_5y6/CdAudio.java
@@ -6,9 +6,9 @@ public class CdAudio extends CD{
 	private String album;
 	private String cantante;
 	
-	public CdAudio(String year, String album, String cantante) {
+	public CdAudio(String year, String album, String cantante, double memoria, boolean rom, boolean dobleCara, String fabricante) {
 		
-		super();
+		super(memoria,rom,dobleCara,fabricante);
 		this.year = year;
 		this.album = album;
 		this.cantante = cantante;
