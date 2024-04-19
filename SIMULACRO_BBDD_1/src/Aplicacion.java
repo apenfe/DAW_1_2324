6648c6d69e78cdc4c4e341c4ac592a1fad5fd2da
apenfe <adrian1414@hotmail.es>
2024-04-19 16:08:55 +0200

    19-04-2024 (DAW) Simulacro examen BBDD

---
+++ SIMULACRO_BBDD_1/src/Aplicacion.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Aplicacion{
+	
+	public static void main(String[] args) {
+		
+		Comida[] lista = importarComida();
+		
+	}
+	
+	public static Comida[] importarComida() {
+		
+		ArrayList<Comida> lista = new ArrayList<>();
+		
+		try {
+			
+			File archivo = new File("assests\\comida.txt");
+			Scanner lector = new Scanner(archivo);
+			
+			while(lector.hasNextLine()) {
+				
+				String[] datos = lector.nextLine().split(",");
+				double dato = Double.parseDouble(datos[1]);
+				
+				lista.add(new Comida(datos[0],dato,datos[2],datos[3]));
+				
+			}
+			
+			
+		} catch (Exception e) {
+			
+			System.err.println(e);
+			
+		}
+		
+		return lista.toArray(new Comida[0]);
+		
+	}
+	
+}
