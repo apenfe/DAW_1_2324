8f780a7a9c502c4dbedb13d3c8726d655406635c
apenfe <adrian1414@hotmail.es>
2024-05-06 16:10:54 +0200

    (DAW) 06/05/2024 GUI

--- GUI/src/ejemplo3/Ventana.java
+++ GUI/src/ejemplo3/Ventana.java
@@ -2,6 +2,10 @@ package ejemplo3;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.event.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -19,6 +23,31 @@ public class Ventana extends JFrame{
 		
 		cp.add(panel);
 		panel.setBackground(Color.blue);
+		
+		// añadir controlador al listener del raton
+		
+		this.addMouseListener(new ControladorRaton());
+		
+		// listener de evento cierre de ventana que finaliza el programa
+		
+		this.addWindowListener(new WindowAdapter() {
+			
+			public void windowClosing(WindowEvent e) {
+				System.out.println("cerrando programa");
+				System.exit(0);
+			}
+			
+		});
+		
+	}
+	
+}
+
+class ControladorRaton extends MouseAdapter{
+	
+	public void mousePressed(MouseEvent e) {
+		
+		System.out.println("clic del raton");
 		
 	}
 	
