72897139ed4c94a807b06393135788ac60c1efe3
apenfe <adrian1414@hotmail.es>
2024-05-06 15:51:08 +0200

    (DAW) 06/05/2024 GUI

---
+++ GUI/src/ejemplo2/Ejemplo02.java
@@ -0,0 +1,15 @@
+package ejemplo2;
+
+import java.awt.Frame;
+
+public class Ejemplo02{
+	
+	public static void main (String[] args) {
+		
+		Frame ventanaPrincipal = new Frame("Ejemplo 1");
+		ventanaPrincipal.setBounds(100, 100, 450, 300);
+		ventanaPrincipal.setVisible(true);
+		
+	}
+
+}
---
+++ GUI/src/ejemplo_1/Ejemplo01.java
@@ -0,0 +1,15 @@
+package ejemplo_1;
+
+import java.awt.Frame;
+
+public class Ejemplo01{
+	
+	public static void main (String args[]) {
+		
+		Frame ventanaPrincipal = new Frame("Primer ejemplo");
+		ventanaPrincipal.setBounds(200, 300, 400, 400);
+		ventanaPrincipal.show();
+		
+		}
+
+}
