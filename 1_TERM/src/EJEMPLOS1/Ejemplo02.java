aa9cddb298bcd86c058ac970d2daf9a9628af06d
Adrian Peñalver <adrian1414@hotmail.es>
2024-07-20 19:01:22 +0200

    Update Ejemplo02.java

--- 1_TERM/src/EJEMPLOS1/Ejemplo02.java
+++ 1_TERM/src/EJEMPLOS1/Ejemplo02.java
@@ -1,5 +1,7 @@
 package EJEMPLOS1;
 
+// crear variable edad y variable nombre. imprimir mi edad es... y mi nombre es...
+
 public class Ejemplo02 {
     public static void main (String[]args){
         int edad = 28;
